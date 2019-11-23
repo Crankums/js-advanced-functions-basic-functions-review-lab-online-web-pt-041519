5e3d9d6edbb877f53bc1fa5fe1523ac6875a0cee
Craig Donavin <crdonavin@gmail.com>
2019-11-23 01:40:32 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,3 +6,10 @@ function saturdayFun(activity = "roller-skate") {
 function mondayWork(activity = "go to the office") {
   return `This Monday, I will ${activity}.`
 }
+
+function wrapAdjective() {
+  return function () {
+    
+  }
+  return
+}
