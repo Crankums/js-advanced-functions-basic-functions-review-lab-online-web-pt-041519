b41227f2d8aeeb3ce13b70df0dff6b3f9649c044
Craig Donavin <crdonavin@gmail.com>
2019-11-23 01:52:25 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -15,6 +15,9 @@ function wrapAdjective(arg1= "*") {
 
 const Calculator ={
   add: function(n1, n2){
-    return n1+n2
+    return n1+n2,
+  },
+  subtract: function(n1, n2) {
+    return n1-n2
   }
 }
